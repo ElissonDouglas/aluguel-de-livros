b9713e8ba70e255cb80d3cb7db98d8096d58bfbd
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2026-04-26 14:06:16 -0300

    initial commit

---
+++ src/main/java/com/learningspringboot/alugueldelivros/AlugueldelivrosApplication.java
@@ -0,0 +1,13 @@
+package com.learningspringboot.alugueldelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugueldelivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugueldelivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningspringboot/alugueldelivros/AlugueldelivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringboot.alugueldelivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugueldelivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
