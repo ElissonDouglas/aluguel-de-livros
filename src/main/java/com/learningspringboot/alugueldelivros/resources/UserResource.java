f7b56f35c579cbf117e354f119d08f8a4270d5e4
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2026-04-27 22:31:13 -0300

    service layer, component registration

--- src/main/java/com/learningspringboot/alugueldelivros/resources/UserResource.java
+++ src/main/java/com/learningspringboot/alugueldelivros/resources/UserResource.java
@@ -2,18 +2,30 @@ package com.learningspringboot.alugueldelivros.resources;
 
 
 import com.learningspringboot.alugueldelivros.entities.User;
+import com.learningspringboot.alugueldelivros.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "João", "joao@gmail.com", "9999999", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
