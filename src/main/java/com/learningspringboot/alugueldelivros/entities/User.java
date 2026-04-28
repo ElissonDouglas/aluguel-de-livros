6ebc779cc71b97bf55609ce5451e6b68998ed65d
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2026-04-27 21:49:37 -0300

    h2 database, test profile, JPA

--- src/main/java/com/learningspringboot/alugueldelivros/entities/User.java
+++ src/main/java/com/learningspringboot/alugueldelivros/entities/User.java
@@ -1,20 +1,25 @@
 package com.learningspringboot.alugueldelivros.entities;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Entity
+@Table(name="tb_user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements Serializable {
     private static final long serialVersionUUID = 1L;
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
