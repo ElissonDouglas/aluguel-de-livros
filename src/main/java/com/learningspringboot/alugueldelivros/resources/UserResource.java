1b04509663218b35a713f41ff9cc609e727e868e
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2026-05-07 20:37:28 -0300

    user update

--- src/main/java/com/learningspringboot/alugueldelivros/resources/UserResource.java
+++ src/main/java/com/learningspringboot/alugueldelivros/resources/UserResource.java
@@ -42,7 +42,12 @@ public class UserResource {
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
-
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
